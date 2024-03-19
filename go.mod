248442f53f65ff6f140635fbcb3a1f0e3218f626
Adam MacDonald <am1macdonald@gmail.com>
2024-03-18 21:27:26 -0400

    Call to get a Location Area

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/am1macdonald/pokedexcli
 
 go 1.22.1
+
+replace github.com/am1macdonald/apiLink v0.0.0 => ./internal/apiLink
+replace github.com/am1macdonald/locationArea v0.0.0 => ./internal/locationArea
+
+require (
+	github.com/am1macdonald/apiLink v0.0.0
+	github.com/am1macdonald/locationArea v0.0.0
+)
--- main.go
+++ main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+
+	"github.com/am1macdonald/pokedexcli/internal/apiLink"
+	"github.com/am1macdonald/pokedexcli/internal/locationArea"
 )
+
+type config struct {
+	next     int
+	previous int
+}
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(config) error
 }
 
-func commandHelp() error {
+func commandHelp(c config) error {
 	fmt.Printf("%v", `
 Welcome to the Pokedex!
 Usage:
@@ -24,7 +33,28 @@ exit: Exit the Pokedex
 	return nil
 }
 
-func commandExit() error {
+func commandMap(c config) error {
+	fmt.Println("Getting next map area")
+	bytes, err := apiLink.FetchMap(c.next)
+	if err != nil {
+		fmt.Println("Something whent whrong?!")
+	}
+	locationArea, err := locationArea.MarshalLocationArea(bytes)
+	if err != nil {
+		fmt.Println("Something whent whrong?!")
+	}
+	fmt.Printf("%v\n", locationArea.Name)
+	return nil
+}
+
+func commandMapB(c config) error {
+	if c.previous == 1 {
+		return errors.New("Already on first page!")
+	}
+	return nil
+}
+
+func commandExit(c config) error {
 	os.Exit(0)
 	return nil
 }
@@ -40,16 +70,30 @@ var commands = map[string]cliCommand{
 		description: "Exit the Pokedex",
 		callback:    commandExit,
 	},
+	"map": {
+		name:        "map",
+		description: "Display map locations",
+		callback:    commandMap,
+	},
+	"mapb": {
+		name:        "mapb",
+		description: "Display previous set of map locations",
+		callback:    commandMapB,
+	},
 }
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
+	conf := config{
+		previous: 0,
+		next:     1,
+	}
 	for {
 		fmt.Print("pokedex > ")
 		for sc.Scan() {
 			text := sc.Text()
 			if _, ok := commands[text]; ok {
-				commands[text].callback()
+				commands[text].callback(conf)
 				break
 			}
 			fmt.Println("unknown command")

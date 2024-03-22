99dc10d5fd9a4f2ada69b46e261db21005b7f429
Adam MacDonald <am1macdonald@gmail.com>
2024-03-22 19:12:15 -0400

    update requirements

--- main.go
+++ main.go
@@ -1,9 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Printf("%v", `
+Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex
+
+`)
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
+
+var commands = map[string]cliCommand{
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+}
+
 func main() {
-	fmt.Println("Hello mom")
+	sc := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("pokedex > ")
+		for sc.Scan() {
+			text := sc.Text()
+			if _, ok := commands[text]; ok {
+				commands[text].callback()
+				break
+			}
+			fmt.Println("unknown command")
+			break
+		}
+	}
 }
